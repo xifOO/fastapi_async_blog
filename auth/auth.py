9afcdf2ef24e2af54956d28fb85bcc2e37152df4
xifOO <wedyi28111@gmail.com>
2023-05-28 01:23:12 +0300

    add new auth logic

--- auth/auth.py
+++ auth/auth.py
@@ -1,20 +1,93 @@
 import config
-from fastapi_users.authentication import BearerTransport, AuthenticationBackend
-from fastapi_users.authentication import JWTStrategy
+from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+from models import models
+from datetime import datetime, timedelta
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+from auth.db import get_async_session
+from schemas.auth_schemas import UserInDB, TokenData
 
 
-bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+SECRET_KEY = config.SECRET
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-SECRET = config.SECRET
+def verify_password(plain_password, hashed_password):
+    """Function for verify password"""
+    return pwd_context.verify(plain_password, hashed_password)
 
 
-def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+def get_password_hash(password):
+    """Function for hashing password"""
+    return pwd_context.hash(password)
 
 
-auth_backend = AuthenticationBackend(
-    name="jwt",
-    transport=bearer_transport,
-    get_strategy=get_jwt_strategy,
-)
+async def get_user(username: str, db: AsyncSession = Depends(get_async_session)):
+    """Async function for get user by username"""
+    try:
+        query = select(models.User).filter(models.User.username == username)
+        result = await db.execute(query)
+        user = result.scalar_one_or_none()
+        if user:
+            return UserInDB(
+                id=user.id,
+                username=user.username,
+                hashed_password=user.hashed_password,
+                email=user.email
+            )
+
+    except SQLAlchemyError as e:
+        """Database query error"""
+        return {"error": str(e)}
+
+
+async def authenticate_user(username: str, password: str, db: AsyncSession = Depends(get_async_session)):
+    """Async function for authenticate user"""
+    user = await get_user(db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """Function for create access token"""
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=config.ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_async_session)):
+    """Async function for get current user by token"""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[config.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = await get_user(username=token_data.username, db=db)
+    if user is None:
+        raise credentials_exception
+    return user
--- auth/db.py
+++ auth/db.py
@@ -1,10 +1,5 @@
 from typing import AsyncGenerator
-from fastapi import Depends
-from fastapi_users.db import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-
-from models.models import User
-
 from config import DB_USER, DB_PASS, DB_PORT, DB_HOST, DB_NAME
 
 
@@ -15,7 +10,3 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
-
-
-async def get_user_db(session: AsyncSession = Depends(get_async_session)):
-    yield SQLAlchemyUserDatabase(session, User)
