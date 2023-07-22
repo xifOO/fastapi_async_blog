fcb1d19627190c82146295d01295d3dd1d03559d
xifOO <wedyi28111@gmail.com>
2023-07-23 00:05:35 +0300

    add auth

--- auth/db.py
+++ auth/db.py
@@ -1,9 +1,9 @@
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from config import DB_USER, DB_PASS, DB_PORT, DB_HOST, DB_NAME
+from config import DATABASE_URL
 
 
-engine = create_async_engine(f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+engine = create_async_engine(DATABASE_URL) # f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
